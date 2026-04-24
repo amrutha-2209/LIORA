ce43c7dbc6192d3e0def212b20737c13a137b86f
Amrutha <amrutha.rammu2005@gmail.com>
2026-04-24 20:47:47 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,22 @@
+package day7;
+
+
+class Student
+{
+	String name1="Amrutha";
+	int marks1=98;
+	String name2="Swathi";
+	int marks2=99;
+}
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		System.out.println(s.name1);
+		System.out.println(s.marks1);
+		System.out.println(s.name2);
+		System.out.println(s.marks2);
+
+	}
+
+}
