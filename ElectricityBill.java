6ba1040f115d2b041f4c90502386fc0c97748bf4
Amrutha <amrutha.rammu2005@gmail.com>
2026-04-27 16:04:52 +0530

    Add files via upload

---
+++ ElectricityBill.java
@@ -0,0 +1,22 @@
+package day7;
+
+public class ElectricityBill {
+
+	public static void main(String[] args) {
+		int units=150;
+		double bill=0;
+		if (units <= 100) {
+            bill = units * 1.5;
+        } 
+        else if (units <= 200) {
+            bill = (100 * 1.5) + ((units - 100) * 2.5);
+        } 
+        else {
+            bill = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4.0);
+        }
+
+        System.out.println("Total Bill = Rs " + bill);
+
+	}
+
+}
