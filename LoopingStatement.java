658594d95759cb25b2a3039199a6a2944a3fe963
Amrutha <amrutha.rammu2005@gmail.com>
2026-04-27 16:03:00 +0530

    Add files via upload

---
+++ LoopingStatement.java
@@ -0,0 +1,32 @@
+package day5;
+
+public class LoopingStatement {
+
+	public static void main(String[] args) {
+		for(int i=146;i<=150;i++)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println("\n");
+		
+		
+		for(int i=5;i>=1;i--)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println("\n");
+		
+		for(int i=2;i<=10;i+=2)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println("\n");
+		
+		for(int i=1;i<=9;i+=2)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
